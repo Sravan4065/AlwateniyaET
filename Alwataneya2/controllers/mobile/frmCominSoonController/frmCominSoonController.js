85ac528f81680610ae26cfd18242373e1d7b1892
GDCIT-LAPT047\Sai Sravan.G <sravansai9312@gmail.com>
2025-04-30 13:23:17 +0530

    details & coming soon screens

---
+++ Alwataneya2/controllers/mobile/frmCominSoonController/frmCominSoonController.js
@@ -0,0 +1,44 @@
+define({ 
+
+  onNavigate: function()
+  {
+    this.view.preShow = this.onPreShow.bind(this);
+    
+  },
+  
+  onPreShow: function(){
+    this.toggleFooterIcons();
+  },
+  
+  toggleFooterIcons: function()
+  {
+   this.view.Footer2.flxHL1.setVisibility(false);
+  this.view.Footer2.flxHL2.setVisibility(false);
+  this.view.Footer2.flxHL3.setVisibility(false);
+  this.view.Footer2.flxHL4.setVisibility(false);
+  this.view.Footer2.flxHL5.setVisibility(false);
+    
+  this.view.Footer2.imgHome.src = "homefooter.png";
+  this.view.Footer2.lblHome.skin = "sknlblCPBoldCapt181818Font100";
+  
+  this.view.Footer2.imgMegaPhone.src = "auctionsfooter.png";
+  this.view.Footer2.lblAuctions.skin = "sknlblCPBoldCapt181818Font100";
+    
+     this.view.Footer2.imgMyBids.src = "mybidsfooter.png";
+   this.view.Footer2.lblMyBids.skin = "sknlblCPBoldCapt181818Font100";
+    
+    var isLogin =voltmx.store.getItem("isLogin");
+
+    var isUserCreated = voltmx.store.getItem("isUserCreated");
+    
+   if(isLogin === true &&  isUserCreated === true){
+      this.view.Footer2.imgSellCar.setVisibility(false);
+      this.view.Footer2.flxProfile.setVisibility(true);
+    }
+    else{
+      this.view.Footer2.imgSellCar.setVisibility(true);
+      this.view.Footer2.flxProfile.setVisibility(false);
+    }
+  }
+
+ });
